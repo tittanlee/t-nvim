8afb717ee4d28e1dbd8aec5e25708151e961f5fa
Titan Lee <titan.lee@amd.com>
2022-02-13 13:13:56 +0800

    Revise the order of init.lua

--- init.lua
+++ init.lua
@@ -5,13 +5,13 @@ require "env_variable"
 -- t_nvim default options
 require "options"
 
--- t_nvim color scehem
-require "theme"
-
 -- t_nvim key mapping
 require "keymappings"
 
 -- t_nvim plugins list
 require "pluginlist"
+
+-- t_nvim color scehem
+require "theme"
 
 
