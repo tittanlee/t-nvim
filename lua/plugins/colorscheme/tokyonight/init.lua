a3340b7878d547f98b62491c6e180981f3012a93
chih-tli <chih-tli@amd.com>
2022-10-11 19:40:26 +0800

    tokyonight parameter fine tune.

--- lua/plugins/colorscheme/tokyonight/init.lua
+++ lua/plugins/colorscheme/tokyonight/init.lua
@@ -13,15 +13,15 @@ end
 tokyonight.setup({
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    style                    = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+    style                    = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
     light_style              = "night", -- The theme is used when the background is set to light
     transparent              = false, -- Enable this to disable setting the background color
     terminal_colors          = true, -- Configure the colors used when opening a `:terminal` in Neovim
     styles                   = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments             = { italic = true },
-        keywords             = { italic = true },
+        keywords             = { italic = true, },
+        comments             = { italic = false, },
         functions            = {},
         variables            = {},
         -- Background styles. Can be "dark", "transparent" or "normal"
@@ -33,7 +33,7 @@ tokyonight.setup({
     sidebars                 = { "qf", "vista_kind", "terminal", "packer", "NvimTree" },
     day_brightness           = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
     hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive             = false, -- dims inactive windows
+    dim_inactive             = true, -- dims inactive windows
     lualine_bold             = false, -- When `true`, section headers in the lualine theme will be bold
 
     --- You can override specific color groups to use other groups or a hex color
