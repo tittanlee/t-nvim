f3c506c23667905b4028876124c82c5c18af026d
chih-tli <chih-tli@amd.com>
2022-10-30 16:39:42 +0800

    set laststatus=3

--- lua/plugins/status_line/heirline/config.lua
+++ lua/plugins/status_line/heirline/config.lua
@@ -12,8 +12,14 @@ end
 local status  = require("plugins.status_line.heirline.status")
 local winbar  = require("plugins.status_line.heirline.winbar")
 
+
+-- 0: never
+-- 1: only if there are at least two windows
+-- 2: always
+-- 3: always and ONLY the last window
+vim.opt.laststatus = 3
+
 heirline.setup(
     status,
     winbar
 )
-
